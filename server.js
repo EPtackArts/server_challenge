b3f6f07c133531212ebc73a1aea944987030cf53
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-08-01 19:43:15 -0500

    4

--- server.js
+++ server.js
@@ -1,32 +1,43 @@
-const express = require('express')
-require("dotenv").config()
-const PORT = process.env.PORT
-const app = express
+// const express = require('express')
+// require("dotenv").config()
+// const PORT = process.env.PORT
+// const app = express
 
-app.get('/', (req, res) => {
-    res.send('welcome')
-})
+// app.get('/', (req, res) => {
+//     res.send('welcome')
+// })
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+// app.listen(PORT, () => console.log(`listening on port ${PORT}`))
 
-const express = require('express')
-require("dotenv").config()
-const PORT = process.env.PORT
-const app = express
+// const express = require('express')
+// require("dotenv").config()
+// const PORT = process.env.PORT
+// const app = express
 
-app.get('/', (req, res) => {
-    res.send('welcome')
-})
+// app.get('/', (req, res) => {
+//     res.send('welcome')
+// })
 
-app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+// app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+// const express = require('express')
+// require('dotenv').config()
+// const PORT = process.env.PORT
+// const app = express
+
+// app.get('/', (req, res) => {
+//     res.send('welcome')
+// })
+
+// app.listen(PORT, ()=> console.log(`listening on PORT ${PORT}`))
 
 const express = require('express')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express
 
-app.get('/', (req, res) => {
+app.get('/', (req, res)=> {
     res.send('welcome')
 })
 
-app.listen(PORT, ()=> console.log(`listening on PORT ${PORT}`))
+app.listen(PORT, ()=> console.log(`listening on port ${PORT}`))

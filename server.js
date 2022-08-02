2239dcafd68d3503bcbe78b2ca59f8109b16a53a
Elizabeth Ptack <101746864+EPtackArts@users.noreply.github.com>
2022-08-01 19:33:02 -0500

    2

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ app.get('/', (req, res) => {
 })
 
 app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+
+const express = require('express')
+require("dotenv").config()
+const PORT = process.env.PORT
+const app = express
+
+app.get('/', (req, res) => {
+    res.send('welcome')
+})
+
+app.listen(PORT, () => console.log(`listening on port ${PORT}`))
